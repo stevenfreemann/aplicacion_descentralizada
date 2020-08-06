c7b3b0a06230e57ce4162255996d5ab3d5af17c5
stevenfreemann <steven.freemann@hotmail.com>
2020-08-05 19:42:50 -0500

    Adicion de url e informacion del candidato al contrato

--- blockchain/contracts/Votacion.sol
+++ blockchain/contracts/Votacion.sol
@@ -6,6 +6,8 @@ contract Votacion{
         uint id;
         string nombre;
         uint cantidad_votos;
+        string url;
+        string info;
     }
     //numero de candidatos
     uint public numero_candidatos;
@@ -15,12 +17,16 @@ contract Votacion{
     mapping(uint => candidato) public candidatos;
     //Inicializa los candidatos
     constructor () public {
-        agregarCandidato("Candidate 1");
-        agregarCandidato("Candidate 2");
+        agregarCandidato("Jorge gonzalez",
+        "images/jorge_gonzalez.jpg",
+        "Estudiante de psicologia-noveno semestre ext Facatativa-25 años");
+        agregarCandidato("Luis bedolla",
+        "images/luis_bedolla.jpg",
+        "Estudiante de Administracion De Empresas-decimo semestre ext Facatativa-24 años");
     }
-    function agregarCandidato (string memory _nombre) private {
+    function agregarCandidato (string memory _nombre,string memory _url,string memory _info) private {
         numero_candidatos++;
-        candidatos[numero_candidatos] = candidato(numero_candidatos, _nombre, 0);
+        candidatos[numero_candidatos] = candidato(numero_candidatos, _nombre, 0,_url,_info);
     }
 
     event votoEvent (
